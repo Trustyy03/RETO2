8087e6dba895ad2dfb50ab3e13d5990abf9d59e4
alu10183093 <magoga2896@gmail.com>
2024-06-06 20:27:04 +0200

    operaciones entidades

---
+++ src/Modelo/Entidades/OperacionesEntidades.java
@@ -0,0 +1,31 @@
+package Modelo.Entidades;
+
+import Controlador.ConexionBDD;
+
+import javax.swing.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OperacionesEntidades {
+    static java.sql.Connection con = ConexionBDD.getInstance().getConnection();
+    public List<Empresa> consultarEmpresas() {
+        List<Empresa> empresas = new ArrayList<>();
+        try {
+            Statement st = con.createStatement();
+            String consultaEmpresas = "SELECT * FROM EMPRESA";
+            ResultSet rs = st.executeQuery(consultaEmpresas);
+
+            while (rs.next()) {
+                Empresa empresa =  new Empresa(rs.getString(1), rs.getString(2), rs.getString(3), rs.getInt(4), rs.getString(5), rs.getString(6));
+                empresas.add(empresa);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showConfirmDialog(null, e.getMessage());
+        }
+
+        return empresas;
+    }
+}

1bdeddfcf7ca5d4038f2c944a5488cd4c8fc68c3
EstebanK13 <esteban.m.mk13@gmail.com>
2024-06-06 16:40:57 +0200

    stackDeLosCojonesArreglado

--- src/Controlador/MainPanelController.java
+++ src/Controlador/MainPanelController.java
@@ -1,71 +1,50 @@
 package Controlador;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.EmptyStackException;
 import java.util.Stack;
 
 public class MainPanelController {
 
     public static JPanel panelActivo, panelPadre, panelInicio;
-
     static Stack<JPanel> listaPaneles = new Stack<>();
 
-    public MainPanelController(JPanel panelPad , JPanel panelActi){
-
+    public MainPanelController(JPanel panelPad, JPanel panelActi) {
         panelPadre = panelPad;
         panelActivo = panelActi;
         panelInicio = panelActivo;
         panelPadre.add(panelActivo);
-        listaPaneles.add(panelActivo);
-
+        if (listaPaneles.isEmpty()) {
+            listaPaneles.add(panelActivo);
+        }
     }
 
-    public static void nuevoPanelActivo(JPanel nuevoPanel){
-
+    public static void nuevoPanelActivo(JPanel nuevoPanel) {
         panelPadre.remove(panelActivo);
         panelPadre.add(nuevoPanel);
         panelPadre.revalidate();
         panelPadre.repaint();
         panelActivo = nuevoPanel;
         listaPaneles.add(panelActivo);
-
     }
 
-    public static void antiguoPanel(){
-
-        if (!listaPaneles.isEmpty()){
-
+    public static void antiguoPanel() {
+        if (listaPaneles.size() > 1) {  // Asegurarnos de que siempre quede al menos un panel (panelInicio)
             listaPaneles.pop();
-
-            if (!listaPaneles.isEmpty()) {
-                panelPadre.remove(panelActivo);
-                panelActivo = listaPaneles.peek();
-                panelPadre.add(panelActivo);
-                panelPadre.revalidate();
-                panelPadre.repaint();
-            }else {
-                panelPadre.remove(panelActivo);
-                panelPadre.add(panelInicio);
-                panelPadre.revalidate();
-                panelPadre.repaint();
-            }
+            panelPadre.remove(panelActivo);
+            panelActivo = listaPaneles.peek();
+            panelPadre.add(panelActivo);
+            panelPadre.revalidate();
+            panelPadre.repaint();
         }
-
-
     }
 
-    public static void volverInicio(){
-
+    public static void volverInicio() {
         panelPadre.remove(panelActivo);
         panelPadre.add(panelInicio);
         panelPadre.revalidate();
         panelPadre.repaint();
         panelActivo = panelInicio;
-        listaPaneles.removeAllElements();
+        listaPaneles.clear();
         listaPaneles.add(panelActivo);
-
     }
-
 }

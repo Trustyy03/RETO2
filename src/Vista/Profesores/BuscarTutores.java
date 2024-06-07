16aa1e2fa6898dce05d520553fab7c6ebbbce238
AymenRabehIsHere <alurab6103@ieselcaminas.org>
2024-06-07 20:07:58 +0200

    interfaces terminadas

--- src/Vista/Profesores/BuscarTutores.java
+++ src/Vista/Profesores/BuscarTutores.java
@@ -1,6 +1,75 @@
 package Vista.Profesores;
 
-import javax.swing.*;
+import Vista.ComponentesGridBagLayout;
 
-public class BuscarTutores extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class BuscarTutores extends JPanel implements ComponentesGridBagLayout{
+
+    JLabel grupoLabel, cursoLabel;
+    JComboBox<String> grupos;
+    JComboBox<String> cursos;
+    JList<String> empresasList;
+    JLabel tutorLabel;
+    JTextField tutorField;
+    GridBagConstraints constraints;
+
+    public BuscarTutores() {
+        setLayout(new GridBagLayout());
+
+        constraints = new GridBagConstraints();
+
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        grupoLabel = new JLabel("GRUPO:");
+        grupos = new JComboBox<>();
+        cursoLabel = new JLabel("CURSO:");
+        cursos = new JComboBox<>();
+
+        tutorLabel = new JLabel("TUTOR:");
+        tutorField = new JTextField();
+
+        empresasList = new JList<>();
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        colocarComponentes();
+    }
+
+    @Override
+    public void colocarComponentes() {
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 1;
+        constraints.weightx = 0.1;
+        constraints.weighty = 0.0;
+        add(grupoLabel,constraints);
+
+        constraints.gridx = 1;
+        add(grupos,constraints);
+
+        constraints.gridx = 2;
+        add(cursoLabel, constraints);
+
+        constraints.gridx = 4;
+        add(cursos, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 1;
+        add(tutorLabel,constraints);
+
+        constraints.gridx = 1;
+        constraints.weightx = 5;
+        add(tutorField,constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 6; //numero de columnas que ocupara
+        constraints.weightx = 1.0; //redimensionamiento
+        constraints.weighty = 1.0;
+        constraints.fill = GridBagConstraints.BOTH;
+        add(new JScrollPane(empresasList), constraints);
+    }
 }
--- src/Vista/Trabajadores/BuscarEmpresaCurso.java
+++ src/Vista/Trabajadores/BuscarEmpresaCurso.java
@@ -1,6 +1,70 @@
 package Vista.Trabajadores;
 
-import javax.swing.*;
+import Controlador.MainPanelController;
+import Modelo.Entidades.Empresa;
+import Vista.ComponentesGridBagLayout;
 
-public class BuscarEmpresaCurso extends JPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class BuscarEmpresaCurso extends JPanel implements ComponentesGridBagLayout {
+
+    JLabel labelEmpresa;
+    JComboBox<Empresa> empresas;
+    JLabel labelCurso;
+    JComboBox<String> cursos;
+    JButton botonListaTrabajadores;
+    GridBagConstraints constraints;
+    ListaDeTrabajadores listaDeTrabajadores;
+
+    public BuscarEmpresaCurso() {
+
+        setLayout(new GridBagLayout());
+        listaDeTrabajadores = new ListaDeTrabajadores();
+
+        constraints = new GridBagConstraints();
+        constraints.insets = new Insets(10, 10, 10, 10);
+
+        labelEmpresa = new JLabel("GRUPO");
+        empresas = new JComboBox<>();
+
+        labelCurso = new JLabel("CURSO");
+        cursos = new JComboBox<>();
+
+        botonListaTrabajadores = new JButton("LISTA DE TRABAJADORES POR EMPRESA / CURSO");
+        botonListaTrabajadores.addActionListener(e ->
+                MainPanelController.nuevoPanelActivo(listaDeTrabajadores)
+        );
+
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+
+        colocarComponentes();
+
+    }
+    @Override
+    public void colocarComponentes(){
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        add(labelEmpresa, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        add(empresas, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        add(labelCurso, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        add(cursos, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        constraints.gridwidth = 2;
+        add(botonListaTrabajadores, constraints);
+
+    }
+
 }
